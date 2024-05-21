73922574b49d88cddca8eec7c083437e63f7e5ad
WD-ASSEM002\duy.vhk <Dyu>
2024-05-21 18:33:35 +0700

    + scriptable object

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private Transform topCenterPoint;
+    [SerializeField] private KitchenObjectsSO kitchenObjectsSO;
     public void Interact()
     {
         Debug.Log("Interact !!" + gameObject.name);
+        var kitchenObjectTransform = Instantiate(kitchenObjectsSO.prefab, topCenterPoint);
+        kitchenObjectTransform.localPosition = Vector3.zero;
     }
 }
